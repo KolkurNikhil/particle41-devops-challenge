48b0f5753992b4d6b025bde9a8671bf02d1b6aa0
KolkurNikhil <63785377+KolkurNikhil@users.noreply.github.com>
2026-04-08 00:02:39 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "particle41-state"
-    key            = "eks/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-  }
 }
 
 provider "aws" {
